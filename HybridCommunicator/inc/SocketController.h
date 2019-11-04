7eebeb86571bdacefd6b31c840220a6fcf6248c0
sy <sy@sy>
2019-11-04 23:58:13 +0300

    Added  minor changes.

--- HybridCommunicator/inc/SocketController.h
+++ HybridCommunicator/inc/SocketController.h
@@ -18,11 +18,31 @@
 #include "Nodes.h"
 
 using namespace std;
+
+#define DATA_SIZE 1024
+#define UART_PACKAGE_SIZE 128
+
+#define TCP_SENDER_PORT 50000
+
+#define UDP_SENDER_PORT 60000
+
+struct Tcp_transfer_data{
+  uint16_t header;
+  uint16_t sequence_package_number;
+  uint8_t data[DATA_SIZE];
+  uint16_t source_port;
+  uint16_t dst_port;
+  uint32_t seq_number;
+  uint32_t ack_number;
+
+};
+
 class SocketController
 {
 	int getSenderIndex( vector<Nodes> _node_list, Tests _test);
 	int getReceiverIndex( vector<Nodes> _node_list, Tests _test);
 	void setTransmissionInformation( vector<Nodes> _node_list,Tests _test);
+	Tcp_transfer_data m_tcp_data;
 	Socket m_socket;
 	Sensor m_sensor;
 	Tcp m_tcp;
